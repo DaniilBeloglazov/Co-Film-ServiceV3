ca8f1c24d6b0aa59ee1c18b3624153f796e3b46a
Beloglazov Daniil <beloglazov.exceedteam@gmail.com>
2022-12-30 13:55:37 +0300

    initial commit

---
+++ src/test/java/com/example/cofilmservicev3/CoFilmServiceV3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cofilmservicev3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoFilmServiceV3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
