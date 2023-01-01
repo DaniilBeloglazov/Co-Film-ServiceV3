91b58f6aea6c0001cb061df0283bee02da28ab71
whiteyesx <beloglazov.exceeteam@gmail.com>
2023-01-01 16:13:41 +0300

    Pageable dont work with @RequestParam

---
+++ src/main/java/com/example/cofilmservicev3/controller/FilmController.java
@@ -0,0 +1,37 @@
+package com.example.cofilmservicev3.controller;
+
+import com.example.cofilmservicev3.model.Film;
+import com.example.cofilmservicev3.service.FilmService;
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1")
+@RequiredArgsConstructor
+public class FilmController {
+
+    private final FilmService filmService;
+
+    private final ModelMapper modelMapper;
+
+    @GetMapping("/films")
+    public ResponseEntity<List<Film>> listAllFilms(@PageableDefault Pageable pageRequest) {
+        //TODO
+        return ResponseEntity.ok().build();
+    }
+
+    @GetMapping("/film/{id}")
+    public ResponseEntity<Film> listFilm(@PathVariable Long id) {
+        //TODO
+        return ResponseEntity.ok().build();
+    }
+}
