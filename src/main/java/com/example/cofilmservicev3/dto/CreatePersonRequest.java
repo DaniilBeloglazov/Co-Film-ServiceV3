5b6e721465283087d3a012a0185691ecc9af793f
Beloglazov Daniil <beloglazov.exceedteam@gmail.com>
2023-01-16 11:10:07 +0300

    Added migration tool & migrations Configured Dockerfile/docker-compose Help file

--- src/main/java/com/example/cofilmservicev3/dto/CreatePersonRequest.java
+++ src/main/java/com/example/cofilmservicev3/dto/CreatePersonRequest.java
@@ -1,17 +1,22 @@
 package com.example.cofilmservicev3.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import java.time.LocalDate;
 
 @Schema(description = "Data transfer object for Person creation.")
-@Data @NoArgsConstructor
+@Data @NoArgsConstructor @AllArgsConstructor
 public class CreatePersonRequest {
     @Schema(description = "Name of the Person.", example = "Dwayne")
     @NotEmpty
@@ -20,15 +25,16 @@ public class CreatePersonRequest {
     @NotEmpty
     private String lastName;
     @Schema(description = "Picture used as Person's poster.")
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     @NotNull
     private MultipartFile poster;
 
-    @Schema(description = "Height of the person", example = "180")
-    @NotEmpty
-    private Double height; // in meters
+    @Schema(description = "Height of the person", example = "180", minimum = "100", maximum = "250")
+    @Min(100) @Max(250)
+    private Long height; // in meters
 
     @Schema(description = "Person's date of birth. The most common ISO Date Format yyyy-MM-dd.", example = "2000-10-31")
     @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
-    @NotEmpty
+    @NotNull
     private LocalDate dateOfBirth;
 }
--- src/test/java/com/example/cofilmservicev3/controller/PersonControllerTest.java
+++ src/test/java/com/example/cofilmservicev3/controller/PersonControllerTest.java
@@ -1,42 +1,53 @@
 package com.example.cofilmservicev3.controller;
 
+import com.example.cofilmservicev3.dto.CreatePersonRequest;
+import com.example.cofilmservicev3.dto.CreatePersonResponse;
 import com.example.cofilmservicev3.model.Person;
 import com.example.cofilmservicev3.repository.projection.PersonProjection;
 import com.example.cofilmservicev3.service.PersonService;
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import lombok.extern.slf4j.Slf4j;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import lombok.val;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.data.jpa.projection.CollectionAwareProjectionFactory;
 import org.springframework.data.projection.ProjectionFactory;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 
+import javax.servlet.http.Part;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
 import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.Map;
 
+@TestPropertySource("classpath:application-test.yml")
 @WebMvcTest(PersonController.class)
 public class PersonControllerTest {
     public PersonControllerTest() throws IOException {
     }
+
     @Autowired
     private MockMvc mockMvc;
 
     @Autowired
-    private ObjectMapper jsonMapper;
+    private ObjectMapper objectMapper;
 
     @MockBean
     private ModelMapper modelMapper;
@@ -44,17 +55,28 @@ public class PersonControllerTest {
     @MockBean
     private PersonService personService;
 
-    private ProjectionFactory factory = new CollectionAwareProjectionFactory();
+    private final ProjectionFactory factory = new CollectionAwareProjectionFactory();
 
     private final String baseUrl = "/api/v1";
-    private final Person mockPerson = new Person(
-            16L, "TestName", "TestLastName", 180.0, LocalDate.of(2000,12,17),
-            22L, "/test/image/test.png", new ArrayList<>(), new ArrayList<>(), new ArrayList<>(), new ArrayList<>());
+    private Person mockPerson;
     private final MockMultipartFile mockImage = new MockMultipartFile(
-            "image",
-            "hello.png",
-            MediaType.MULTIPART_FORM_DATA_VALUE,
-            Files.readAllBytes(Path.of("/home/user/Documents/Education/Co-Film-ServiceV3/src/main/resources/static/testImage.jpeg")));
+            "poster", "testImage.jpeg", MediaType.MULTIPART_FORM_DATA_VALUE, new ClassPathResource("static/testImage.jpeg").getInputStream());
+
+    private CreatePersonRequest mockCreateRequest;
+
+    @BeforeEach
+    public void beforeEachCall() throws IOException {
+
+        objectMapper.registerModule(new JavaTimeModule());
+
+        this.mockPerson = new Person(
+                16L, "TestName", "TestLastName", 180.0, LocalDate.of(2000, 12, 17),
+                null, "/test/image/test.png", new ArrayList<>(), new ArrayList<>(), new ArrayList<>(), new ArrayList<>());
+
+        this.mockCreateRequest = new CreatePersonRequest(
+                "TestName", "TestLastName", mockImage, 200L, LocalDate.of(17, 12, 12));
+
+    }
 
     @Test
     public void invalidIdArgument() throws Exception {
@@ -71,7 +93,7 @@ public class PersonControllerTest {
         System.out.println("======================================================================================");
 
         mockMvc.perform(MockMvcRequestBuilders.multipart(
-                HttpMethod.PUT,baseUrl + "/persons/qweqwe/photos")
+                                HttpMethod.PUT, baseUrl + "/persons/qweqwe/photos")
                         .file(mockImage))
                 .andDo(MockMvcResultHandlers.print())
                 .andExpect(MockMvcResultMatchers.status().isBadRequest());
@@ -83,18 +105,61 @@ public class PersonControllerTest {
     }
 
     @Test
-    public void listPersonsTest() throws Exception {
+    public void listPersonTest() throws Exception {
 
-        Mockito.when(personService.getPerson(16L)).thenReturn(factory.createProjection(PersonProjection.class, mockPerson));
-
-        val expectedContent = jsonMapper.writeValueAsString(factory.createProjection(PersonProjection.class, mockPerson));
+        Mockito.when(personService.getPerson(mockPerson.getId()))
+                .thenReturn(factory.createProjection(PersonProjection.class, mockPerson));
 
         mockMvc.perform(MockMvcRequestBuilders.get(baseUrl + "/persons/16"))
                 .andDo(MockMvcResultHandlers.print())
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().json("" +
+                        "{\n" +
+                        "  \"id\": 16,\n" +
+                        "  \"name\": \"TestName\",\n" +
+                        "  \"avatarUri\": \"/test/image/test.png\",\n" +
+                        "  \"lastName\": \"TestLastName\",\n" +
+                        "  \"photos\": [],\n" +
+                        "  \"age\": 22,\n" +
+                        "  \"height\": 180,\n" +
+                        "  \"dateOfBirth\": [\n" +
+                        "    2000,\n" +
+                        "    12,\n" +
+                        "    17\n" +
+                        "  ],\n" +
+                        "  \"directedFilms\": [],\n" +
+                        "  \"writtenFilms\": [],\n" +
+                        "  \"actoredFilms\": []\n" +
+                        "}\n"));
+    }
+
+    @Test
+    public void addPersonTest() throws Exception {
+
+        Mockito.when(modelMapper.map(mockCreateRequest, Person.class)).thenReturn(mockPerson);
+        Mockito.when(personService.createPerson(mockPerson, mockImage)).thenReturn(228L);
+
+        val requestParams = convertToMultiValueMap(mockCreateRequest);
+
+        mockMvc.perform(MockMvcRequestBuilders.multipart(HttpMethod.POST, baseUrl + "/person")
+                        .file(mockImage)
+                        .params(requestParams))
+                .andDo(MockMvcResultHandlers.print())
                 .andExpect(MockMvcResultMatchers.status()
-                        .isOk())
-                .andExpect(MockMvcResultMatchers.content().string(expectedContent)
-                );
+                        .isCreated())
+                .andExpect(MockMvcResultMatchers.content().json("" +
+                        "{\n" +
+                        "  \"id\": 228\n" +
+                        "}"));
+
+
+    }
+
+    private <T> MultiValueMap<String, String> convertToMultiValueMap(T object) {
+        Map<String, String> fieldMap = objectMapper.convertValue(object, new TypeReference<>() {});
+        MultiValueMap<String, String> multiValueMap = new LinkedMultiValueMap<>();
+        multiValueMap.setAll(fieldMap);
+        return multiValueMap;
     }
 
 }
